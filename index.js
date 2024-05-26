545985fcbc87e70270fefc5fad6f39691c005e9a
Gerald Andrews <gerald2399@comcast.net>
2024-05-26 12:12:27 -0400

    Changed public folder name to docs

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.set('trust proxy', 1)
 app.use(cors())
 
 // Set static folder
-app.use(express.static('public'))
+app.use(express.static('docs'))
 
 // Routes
 app.use('/api', require('./routes'))
